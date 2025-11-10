602cc6727b3f8fd4102fedaeb4ddd81a45ed0751
Muqiao Lei <rsdarven@gmail.com>
2025-11-10 02:50:49 -0500

    base code and skybox edited

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Rendering;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform brush;
+    public Animator brushAnimator;
+    public Camera brushCamera;
+
+    public Volume grainVolume;
+    public Renderer drawingRenderer;
+    public CinemachineFreeLook freeLook;
+
+    public bool isDrawing;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+
+        if (brushCamera != null)
+            brushCamera.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        Vector3 temp = Input.mousePosition;
+        temp.z = .4f;
+        if(isDrawing)
+            brush.position = Vector3.Lerp(brush.position,Camera.main.ScreenToWorldPoint(temp),.5f);
+        ClampPosition(brush);
+
+        if (Input.GetKeyDown(KeyCode.C))
+            SetDrawingMode(!isDrawing);
+
+        if (Input.GetKeyUp(KeyCode.C))
+            SetDrawingMode(!isDrawing);
+
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+
+        if(brushAnimator != null && brushAnimator.gameObject.activeSelf)
+        {
+            brushAnimator.SetFloat("X", Mathf.Lerp(brushAnimator.GetFloat("X"), Input.GetAxis("Mouse X") * 1, .07f));
+            brushAnimator.SetFloat("Y", Mathf.Lerp(brushAnimator.GetFloat("Y"), Input.GetAxis("Mouse Y") * 1, .07f));
+            brushAnimator.SetBool("isDrawing", Input.GetMouseButton(0));
+        }
+    }
+
+    void ClampPosition(Transform obj)
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(obj.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        obj.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+    public void SetDrawingMode(bool state)
+    {
+        Cursor.lockState = state ? CursorLockMode.None : CursorLockMode.Locked;
+
+        if (state == true)
+        {
+            Camera.main.cullingMask &= ~(1 << LayerMask.NameToLayer("Default"));
+            Camera.main.cullingMask &= ~(1 << LayerMask.NameToLayer("Interactables"));
+            //brush.GetChild(0).localPosition.Set(brush.GetChild(0).localPosition.x, 0.17f, brush.GetChild(0).localPosition.z);
+            brush.GetChild(0).DOLocalMoveY(0.17f, .3f).SetUpdate(true).From();
+        }
+        else
+        {
+            Camera.main.cullingMask |= 1 << LayerMask.NameToLayer("Default");
+            Camera.main.cullingMask |= 1 << LayerMask.NameToLayer("Interactables");
+        }
+
+        isDrawing = state;
+
+        //determine if time is running or not
+        Time.timeScale = isDrawing ? 0 : 1;
+        //determine if the freelook camera is active
+        freeLook.enabled = !state;
+
+        drawingRenderer.enabled = state;
+        drawingRenderer.transform.GetChild(0).gameObject.SetActive(state);
+        brushCamera.gameObject.SetActive(state);
+
+        //effects
+        float effectAmount = isDrawing ? 1 : 0;
+        drawingRenderer.transform.DOLocalRotate(new Vector3(isDrawing ? 60 : 90, 180,0), .5f, RotateMode.Fast).SetUpdate(true);
+        DOVirtual.Float(grainVolume.weight, effectAmount, .5f, (x) => grainVolume.weight = x).SetUpdate(true);
+        drawingRenderer.material.DOFloat(effectAmount, "SepiaAmount", .5f).SetUpdate(true);
+
+        if(state == false)
+        {
+            FindObjectOfType<Demo>().TryRecognize();
+        }
+    }
+}
---
+++ Assets/Scripts/SkyboxController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class SkyboxController : MonoBehaviour
+{
+    public Material daySkybox;
+    public Material nightSkybox;
+    public Light sunLight;
+    public float transitionSpeed = 0.5f;
+
+    private bool isDay = true;
+    private float blendValue = 1f; 
+    private float targetBlend = 1f;
+    private float maxLightIntensity;
+
+
+    private Color daySunDisc, nightSunDisc;
+    private Color daySunHalo, nightSunHalo;
+    private Color dayHorizon, nightHorizon;
+    private Color daySkyTop, nightSkyTop;
+    private Color daySkyBottom, nightSkyBottom;
+
+    void Start()
+    {
+        maxLightIntensity = sunLight.intensity;
+
+
+        daySunDisc = daySkybox.GetColor("_SunDiscColor");
+        daySunHalo = daySkybox.GetColor("_SunHaloColor");
+        dayHorizon = daySkybox.GetColor("_HorizonLineColor");
+        daySkyTop = daySkybox.GetColor("_SkyGradientTop");
+        daySkyBottom = daySkybox.GetColor("_SkyGradientBottom");
+
+        nightSunDisc = nightSkybox.GetColor("_SunDiscColor");
+        nightSunHalo = nightSkybox.GetColor("_SunHaloColor");
+        nightHorizon = nightSkybox.GetColor("_HorizonLineColor");
+        nightSkyTop = nightSkybox.GetColor("_SkyGradientTop");
+        nightSkyBottom = nightSkybox.GetColor("_SkyGradientBottom");
+
+
+        RenderSettings.skybox = daySkybox;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SetDay();
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            SetNight();
+        }
+
+
+        blendValue = Mathf.Lerp(blendValue, targetBlend, Time.deltaTime * transitionSpeed);
+
+
+        Color currentSunDisc = Color.Lerp(nightSunDisc, daySunDisc, blendValue);
+        Color currentSunHalo = Color.Lerp(nightSunHalo, daySunHalo, blendValue);
+        Color currentHorizon = Color.Lerp(nightHorizon, dayHorizon, blendValue);
+        Color currentTop = Color.Lerp(nightSkyTop, daySkyTop, blendValue);
+        Color currentBottom = Color.Lerp(nightSkyBottom, daySkyBottom, blendValue);
+
+
+        Material currentSkybox = RenderSettings.skybox;
+        currentSkybox.SetColor("_SunDiscColor", currentSunDisc);
+        currentSkybox.SetColor("_SunHaloColor", currentSunHalo);
+        currentSkybox.SetColor("_HorizonLineColor", currentHorizon);
+        currentSkybox.SetColor("_SkyGradientTop", currentTop);
+        currentSkybox.SetColor("_SkyGradientBottom", currentBottom);
+
+
+        sunLight.intensity = Mathf.Lerp(0.1f, maxLightIntensity, blendValue);
+
+        DynamicGI.UpdateEnvironment();
+    }
+
+    public void SetDay()
+    {
+        isDay = true;
+        targetBlend = 1f;
+    }
+
+    public void SetNight()
+    {
+        isDay = false;
+        targetBlend = 0f;
+    }
+}
